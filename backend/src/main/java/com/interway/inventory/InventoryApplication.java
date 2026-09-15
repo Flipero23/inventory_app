7edab4d439efc1882e726ad689875d524ffcdabe
Gorazd Filipovski <fgorazd@yahoo.com>
2026-09-15 23:47:37 +0200

    Updated pagination with stable page serialization

--- backend/src/main/java/com/interway/inventory/InventoryApplication.java
+++ backend/src/main/java/com/interway/inventory/InventoryApplication.java
@@ -2,8 +2,10 @@ package com.interway.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class InventoryApplication {
 
 	public static void main(String[] args) {
