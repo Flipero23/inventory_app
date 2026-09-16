b8f62df80e9cbc1a1599211aad4844c0d468cfa8
Gorazd Filipovski <fgorazd@yahoo.com>
2026-09-16 16:04:33 +0200

    Added Angular Material and converted the product list to Material table

--- frontend/src/app/components/product-list/product-list.ts
+++ frontend/src/app/components/product-list/product-list.ts
@@ -3,10 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { Product } from '../../models/product.model';
 import { ProductService } from '../../services/product.service';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-product-list',
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MatTableModule, MatButtonModule, MatProgressSpinnerModule, MatIconModule],
   templateUrl: './product-list.html',
   styleUrl: './product-list.css'
 })
@@ -17,6 +21,8 @@ export class ProductList implements OnInit {
   products = signal<Product[]>([]);
   isLoading = signal(false);
   errorMessage = signal('');
+
+   displayedColumns: string[] = ['name', 'category', 'price', 'quantityInStock', 'actions'];
 
   constructor(private productService: ProductService) {}
 
