8cb93098f96ef895c7808e7e11ba931cc4c0bf3d
Gorazd Filipovski <fgorazd@yahoo.com>
2026-09-16 17:49:08 +0200

    Added product detail component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { ProductList } from './components/product-list/product-list';
 import { ProductForm } from './components/product-form/product-form';
+import { ProductDetail } from './components/product-detail/product-detail';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'products', pathMatch: 'full' },
     { path: 'products', component: ProductList },
     { path: 'products/new', component: ProductForm },
-    { path: 'products/:id/edit', component: ProductForm }
+    { path: 'products/:id/edit', component: ProductForm },
+    { path: 'products/:id', component: ProductDetail }
 ];
---
+++ frontend/src/app/components/product-detail/product-detail.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { Product } from '../../models/product.model';
+import { ProductService } from '../../services/product.service';
+
+@Component({
+  selector: 'app-product-detail',
+  imports: [CommonModule, RouterModule, MatCardModule, MatButtonModule],
+  templateUrl: './product-detail.html',
+  styleUrl: './product-detail.css'
+})
+export class ProductDetail implements OnInit {
+
+  product = signal<Product | null>(null);
+  isLoading = signal(false);
+  errorMessage = signal('');
+
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService
+  ) {}
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.isLoading.set(true);
+
+    this.productService.getById(id).subscribe({
+      next: (product) => {
+        this.product.set(product);
+        this.isLoading.set(false);
+      },
+      error: () => {
+        this.errorMessage.set('Производот не е пронајден.');
+        this.isLoading.set(false);
+      }
+    });
+  }
+}
